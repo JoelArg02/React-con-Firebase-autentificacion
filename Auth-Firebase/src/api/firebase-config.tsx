a88ef20c267aefa11f4fe64bd7b383645c634dbe
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2024-02-03 00:03:54 -0500

    prueba terminada

--- Auth-Firebase/src/api/firebase-config.tsx
+++ Auth-Firebase/src/api/firebase-config.tsx
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyC_SySb62qDdppRjKf9DJp6nM4UK9Npsqc",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app); // Inicializa y exporta db
